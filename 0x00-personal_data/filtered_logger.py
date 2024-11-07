94251c800d56984860a98ca101765e0963d8b3f9
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-07 23:42:30 +0200

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+""" filtered logger """
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
