f903446b4a2404878e5edcf9b8732f851c7251bc
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-14 20:22:15 +0200

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from .auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -27,3 +28,8 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ return user based on cookie value """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
