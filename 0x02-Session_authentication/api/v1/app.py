cb0ee4562754261dcecdaf7449d2ee11b39d81a9
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-14 20:14:28 +0200

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,16 +57,19 @@ def authenticate_user():
             '/api/v1/forbidden/',
             '/api/v1/auth_session/login/',
         ]
-        if auth.authorization_header(request) and auth.session_cookie(request):
-            abort(401)
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
+            session_cookie = auth.session_cookie(request)
             request.current_user = user
-            if auth_header is None:
+            if auth_header and session_cookie:
+                abort(401)
+            if not auth_header and not session_cookie:
                 abort(401)
             if user is None:
                 abort(403)
+            if auth_header is None:
+                abort(401)
 
 
 if __name__ == "__main__":
