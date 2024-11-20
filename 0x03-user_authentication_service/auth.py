1d36f355201f6d148e6af5447fc8d8f49a6f519d
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-20 22:22:07 +0200

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,30 @@
 """
 import bcrypt
 
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ handle basic data validation and hashing
+        """
+        my_db = self._db
+        try:
+            found_user = my_db.find_user_by(email=email)
+            raise ValueError(f"User {found_user.email} already exists")
+        except NoResultFound:
+            password = _hash_password(password)
+            new_user = my_db.add_user(email=email, hashed_password=password)
+            return new_user
+
 
 def _hash_password(password: str) -> bytes:
     """ return a salted hash of the input password
