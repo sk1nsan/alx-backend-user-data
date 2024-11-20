1fac1fc855dd3557e8a9916d856acc5946f3eefa
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-20 21:25:15 +0200

    taslk 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,3 +39,9 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """ Find users by given keyword arguments
+        """
+        found_user = self._session.query(User).filter_by(**kwargs).first()
+        return found_user
