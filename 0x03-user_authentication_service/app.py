7954c2376912d6077ffd56eba51f9e354176b58c
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-21 09:50:33 +0200

    task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -85,5 +85,19 @@ def get_reset_password_token() -> Response:
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> Response:
+    """ route for updatting password
+    """
+    email = request.form['email']
+    reset_token = request.form['reset_token']
+    new_password = request.form['new_password']
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": f"{email}", "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
