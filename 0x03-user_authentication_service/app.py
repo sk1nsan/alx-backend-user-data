e5d6a88a8f0996250583802db84b5b732c301fbf
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-21 09:42:16 +0200

    task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,5 +73,17 @@ def profile() -> Response:
     abort(403)
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> Response:
+    """ route for resetting password
+    """
+    email = request.form['email']
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": f"{email}", "reset_token": f"{reset_token}"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
